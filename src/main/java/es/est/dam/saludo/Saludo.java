b0d12d275f3b8667b68c8142cebf231e1af8ad84
anasoulgha <anas.oulghazi@educa.madrid.org>
2025-11-26 14:35:00 -0600

    commit2

--- src/main/java/es/est/dam/saludo/Saludo.java
+++ src/main/java/es/est/dam/saludo/Saludo.java
@@ -2,25 +2,23 @@ package es.est.dam.saludo;
 
 import java.util.concurrent.Exchanger;
 
-
-
-public class Saludo implements Runnable{
-    private static final String SALUDO= "Hola";
-    private static final String DESPEDIDA ="Adios";
+public class Saludo implements Runnable {
+    private static final String SALUDO = "Hola";
+    private static final String DESPEDIDA = "Adios";
 
     private Exchanger<String> exchanger;
-     String mensaje;
+    String mensaje;
 
-     public Saludo(Exchanger<String> exchanger) {
+    public Saludo(Exchanger<String> exchanger) {
         this.exchanger = exchanger;
-        
+
     }
 
-  public void run() {
+    public void run() {
         try {
-           mensaje = exchanger.exchange(SALUDO);
-           System.out.println(mensaje);
-           System.out.println(DESPEDIDA);
+            mensaje = exchanger.exchange(SALUDO);
+            System.out.println(mensaje);
+            System.out.println(DESPEDIDA);
         } catch (InterruptedException e) {
             System.err.println(e.getMessage());
         }
